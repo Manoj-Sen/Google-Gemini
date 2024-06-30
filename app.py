a69097d88be0904f63bb9fefefa37eee654bcc1d
Manoj Sen <manu.sen2705@gmail.com>
2024-06-30 13:40:48 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,40 @@
+import os
+import streamlit as st
+from dotenv import load_dotenv
+load_dotenv()
+
+import google.generativeai as genai
+import textwrap
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+
+from IPython.display import Markdown
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+
+def get_gemini_response(question):
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(question)
+    return response.text
+
+##initialize our streamlit app
+
+st.set_page_config(page_title="Q&A Demo")
+
+st.header("Gemini Application")
+
+input=st.text_input("Input: ",key="input")
+
+
+submit=st.button("Ask the question")
+
+## If ask button is clicked
+
+if submit:
+    
+    response=get_gemini_response(input)
+    st.subheader("The Response is")
+    st.write(response)
